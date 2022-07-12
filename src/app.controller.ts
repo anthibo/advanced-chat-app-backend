1389f36df8d8cd36e1d6f55b523513ea5cfa05f9
anthibo <anthiboSama@gmail.com>
2022-07-12 05:25:40 +0200

    remove useGuard from app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { JwtAuthGuard } from './modules/auth/guards/jwt.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   getHello(): string {
     return this.appService.getHello();
